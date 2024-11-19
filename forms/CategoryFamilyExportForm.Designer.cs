64704160d8f65c6f92ec14f15321d522e273bc6b
Robson Perdigão <robson.perdigao@outlook.com>
2024-11-19 15:31:45 -0300

    forms family

---
+++ forms/CategoryFamilyExportForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Solutia.Forms
+{
+    partial class CategoryFamilyExportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clbCategories = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // clbCategories
+            // 
+            this.clbCategories.FormattingEnabled = true;
+            this.clbCategories.Location = new System.Drawing.Point(79, 78);
+            this.clbCategories.Name = "clbCategories";
+            this.clbCategories.Size = new System.Drawing.Size(281, 89);
+            this.clbCategories.TabIndex = 0;
+            // 
+            // CategoryFamilyExportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clbCategories);
+            this.Name = "CategoryFamilyExportForm";
+            this.Text = "CategoryFamilyExportForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clbCategories;
+    }
+}
